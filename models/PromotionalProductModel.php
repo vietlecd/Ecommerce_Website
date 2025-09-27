6c67ae51481f933259e63dbad3de9dfa63552228
vietlecd <viet.lekhmtk22@hcmut.edu.vn>
2025-09-27 23:14:29 +0700

    fix promotion page

--- models/PromotionalProductModel.php
+++ models/PromotionalProductModel.php
@@ -1,17 +1,20 @@
 <?php
 require_once 'models/ProductModelv2.php';
 
-class PromotionalProductModel {
+class PromotionalProductModel
+{
     private $productModel;
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->productModel = new ProductModel();
         $database = new Database();
         $this->db = $database->getConnection();
     }
 
-    public function getAllProducts() {
+    public function getAllProducts()
+    {
         $products = $this->productModel->getAllProducts();
         foreach ($products as &$product) {
             $product['promotion'] = $this->getPromotionForProduct($product['id']);
@@ -20,7 +23,8 @@ class PromotionalProductModel {
         return $products;
     }
 
-    public function getProductById($id) {
+    public function getProductById($id)
+    {
         $product = $this->productModel->getProductById($id);
         if ($product) {
             $product['promotion'] = $this->getPromotionForProduct($id);
@@ -29,23 +33,26 @@ class PromotionalProductModel {
         return $product;
     }
 
-    private function getPromotionForProduct($product_id) {
+    private function getPromotionForProduct($product_id)
+    {
         $current_date = date('Y-m-d H:i:s');
         $query = "SELECT p.* 
                   FROM promotions p 
                   JOIN promotion_shoes ps ON p.promotion_id = ps.promotion_id 
                   WHERE ps.shoe_id = :shoe_id 
-                  AND p.start_date <= :current_date 
-                  AND p.end_date >= :current_date";
-        
+                  AND p.start_date <= :start_date 
+                  AND p.end_date >= :end_date";
+
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':shoe_id', (int)$product_id, PDO::PARAM_INT);
-        $stmt->bindValue(':current_date', $current_date, PDO::PARAM_STR);
+        $stmt->bindValue(':start_date', $current_date, PDO::PARAM_STR);
+        $stmt->bindValue(':end_date', $current_date, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    private function calculateDiscountedPrice($product, $promotion) {
+    private function calculateDiscountedPrice($product, $promotion)
+    {
         if (!empty($promotion)) {
             if ($promotion['discount_percentage']) {
                 return $product['price'] * (1 - $promotion['discount_percentage'] / 100);
@@ -56,7 +63,8 @@ class PromotionalProductModel {
         return $product['price'];
     }
 
-    public function getAllPromotions($limit = 10, $offset = 0, $keyword = '', $sort = 'ASC') {
+    public function getAllPromotions($limit = 10, $offset = 0, $keyword = '', $sort = 'ASC')
+    {
         $query = "SELECT * FROM promotions WHERE 1=1";
         $params = [];
 
@@ -78,7 +86,8 @@ class PromotionalProductModel {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getPromotionsCount($keyword = '') {
+    public function getPromotionsCount($keyword = '')
+    {
         $query = "SELECT COUNT(*) FROM promotions WHERE 1=1";
         $params = [];
 
@@ -95,7 +104,8 @@ class PromotionalProductModel {
         return $stmt->fetchColumn();
     }
 
-    public function getPromotionById($promotionId) {
+    public function getPromotionById($promotionId)
+    {
         $query = "SELECT * FROM promotions WHERE promotion_id = :promotion_id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':promotion_id', (int)$promotionId, PDO::PARAM_INT);
@@ -103,7 +113,8 @@ class PromotionalProductModel {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function getProductsByPromotionId($promotionId) {
+    public function getProductsByPromotionId($promotionId)
+    {
         $query = "SELECT shoe_id FROM promotion_shoes WHERE promotion_id = :promotion_id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':promotion_id', (int)$promotionId, PDO::PARAM_INT);
@@ -111,14 +122,16 @@ class PromotionalProductModel {
         return array_column($stmt->fetchAll(PDO::FETCH_ASSOC), 'shoe_id');
     }
 
-    public function removeAllProductsFromPromotion($promotionId) {
+    public function removeAllProductsFromPromotion($promotionId)
+    {
         $query = "DELETE FROM promotion_shoes WHERE promotion_id = :promotion_id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':promotion_id', (int)$promotionId, PDO::PARAM_INT);
         $stmt->execute();
     }
 
-    public function assignProductToPromotion($promotionId, $productId) {
+    public function assignProductToPromotion($promotionId, $productId)
+    {
         $this->removeProductFromAllPromotions($productId);
 
         $query = "INSERT INTO promotion_shoes (promotion_id, shoe_id) VALUES (:promotion_id, :shoe_id)";
@@ -128,14 +141,16 @@ class PromotionalProductModel {
         $stmt->execute();
     }
 
-    public function removeProductFromAllPromotions($productId) {
+    public function removeProductFromAllPromotions($productId)
+    {
         $query = "DELETE FROM promotion_shoes WHERE shoe_id = :shoe_id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':shoe_id', (int)$productId, PDO::PARAM_INT);
         $stmt->execute();
     }
 
-    public function createPromotion($name, $startDate, $endDate, $discountPercentage, $fixedPrice, $promotionType) {
+    public function createPromotion($name, $startDate, $endDate, $discountPercentage, $fixedPrice, $promotionType)
+    {
         $query = "INSERT INTO promotions (promotion_name, start_date, end_date, discount_percentage, fixed_price, promotion_type) 
                   VALUES (:promotion_name, :start_date, :end_date, :discount_percentage, :fixed_price, :promotion_type)";
         $stmt = $this->db->prepare($query);
@@ -148,7 +163,8 @@ class PromotionalProductModel {
         $stmt->execute();
     }
 
-    public function updatePromotion($promotionId, $name, $startDate, $endDate, $discountPercentage, $fixedPrice, $promotionType) {
+    public function updatePromotion($promotionId, $name, $startDate, $endDate, $discountPercentage, $fixedPrice, $promotionType)
+    {
         $query = "UPDATE promotions 
                   SET promotion_name = :promotion_name, 
                       start_date = :start_date, 
@@ -168,7 +184,8 @@ class PromotionalProductModel {
         $stmt->execute();
     }
 
-    public function deletePromotion($promotionId) {
+    public function deletePromotion($promotionId)
+    {
         $query = "UPDATE news SET promotion_id = NULL WHERE promotion_id = :promotion_id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':promotion_id', (int)$promotionId, PDO::PARAM_INT);
